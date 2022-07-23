979fa68aa66e1c65a7e6af91614b05d1f99c719a
CHOI DAE GEON <chleorjs37@gmail.com>
2022-07-23 14:50:34 +0900

    scss 적용

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
 				<Field id="name" name="name" label="강사명" rules="required" v-slot="{ field, errors }">
 					<div class="row h-100px">
 						<div class="col-3 carrot-form-label center">강사명</div>
-						<div class="col-9 carrot-form-control center pt-15px">
-							<div class="w-100per h-60px">
-								<input type="text" v-bind="field" class="form-control" placeholder="Username" />
-								<div :class="errors[0] ? 'error' : ''" class="dp-block mh-20px lh-20px">{{ errors[0] }}</div>
+						<div class="col-9 carrot-form-control center">
+							<div class="w-100per h-60px mt-15px">
+								<input type="text" v-bind="field" class="form-control" placeholder="강사명" />
+								<div :class="errors[0] ? 'error' : ''" class="dp-block h-20px lh-20px">{{ errors[0] }}</div>
 							</div>
 						</div>
 					</div>
@@ -18,10 +18,10 @@
 				<Field id="kname" name="kname" label="국문명" rules="required" v-slot="{ field, errors }">
 					<div class="row h-100px">
 						<div class="col-3 col-sm-3 carrot-form-label center">국문명</div>
-						<div class="col-9 col-sm-9 carrot-form-control center pt-15px">
-							<div class="w-100per h-60px">
-								<input type="text" v-bind="field" class="form-control" placeholder="Username" />
-								<div :class="errors[0] ? 'error' : ''" class="dp-block mh-20px lh-20px">{{ errors[0] }}</div>
+						<div class="col-9 col-sm-9 carrot-form-control center">
+							<div class="w-100per h-60px mt-15px">
+								<input type="text" v-bind="field" class="form-control" placeholder="국문명" />
+								<div :class="errors[0] ? 'error' : ''" class="dp-block h-20px lh-20px">{{ errors[0] }}</div>
 							</div>
 						</div>
 					</div>
@@ -34,10 +34,10 @@
 				<Field id="eName" name="eName" label="영문명" rules="required" v-slot="{ field, errors }">
 					<div class="row h-100px">
 						<div class="col-3 col-lg-1 carrot-form-label center">영문명</div>
-						<div class="col-9 col-lg-11 carrot-form-control center pt-15px">
-							<div class="w-100per h-60px">
-								<input type="text" v-bind="field" class="form-control" placeholder="Username" />
-								<div :class="errors[0] ? 'error' : ''" class="dp-block mh-20px lh-20px">{{ errors[0] }}</div>
+						<div class="col-9 col-lg-11 carrot-form-control center">
+							<div class="w-100per h-60px mt-15px">
+								<input type="text" v-bind="field" class="form-control" placeholder="영문명" />
+								<div :class="errors[0] ? 'error' : ''" class="dp-block h-20px lh-20px">{{ errors[0] }}</div>
 							</div>
 						</div>
 					</div>
@@ -54,7 +54,6 @@ export default {
 </script>
 
 <style lang="scss">
-// .h-60px {
-// 	height: 60px;
-// }
+/*
+*/
 </style>
